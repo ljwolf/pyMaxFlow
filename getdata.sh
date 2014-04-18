2b8a0965a7e25e9d5939442d587ecade02eba4f5
Levi John Wolf <ljw@dane.local>
2014-04-18 02:24:08 -0700

    changed wget to curl for OSXers

--- getdata.sh
+++ getdata.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget http://www.public.asu.edu/~lwolf2/data/example_data.tar.gz && tar -zxvf example_data.tar.gz
+curl http://www.public.asu.edu/~lwolf2/data/example_data.tar.gz > example_data.tar.gz && tar -zxvf example_data.tar.gz
